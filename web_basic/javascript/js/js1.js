ecd6be6d71ba05e09585f25e85d05cd488aa2f1d
smj0104 <hikoseki1997@naver.com>
2023-03-13 15:16:41 +0900

    0313 javascript 정리

---
+++ web_basic/javascript/js/js1.js
@@ -0,0 +1,16 @@
+//alert("알림창을 확인해주세요.");
+//alert("두번째 알림");
+//alert(120);
+
+//console.log("println");
+//let flag = confirm("선택해주세요");
+//console.log(flag);
+//let name2 = prompt("이름을 입력해주세요");
+//console.let(name2);
+
+let test;
+console.log(123)
+
+let num1 = parseInt(prompt("숫자덧셈1"));
+let num2 = parseInt(prompt("숫자덧셈2"));
+console.log(num1 + num2);
